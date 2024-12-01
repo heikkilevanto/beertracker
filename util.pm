d74d3e1c658ccb26a1fd708b70828613e037f564
Heikki Levanto <heikki@lsd.dk>
2024-12-01 17:46:04 +0100

    Location insert with the new dropdown helper works

--- util.pm
+++ util.pm
@@ -142,5 +142,137 @@ sub listsmenu {
 
 
 ################################################################################
+# Drop-down selection with filtering
+################################################################################
+# example:
+#  dropdown( "location", $current_location_id, $current_location_name,
+#    "<div class='dropdown-item' id='new'>(new)</div>\n" .
+#    "<div class='dropdown-item' id='6'>Ølbaren</div>\n" .
+#    ... );
+# Omit the "new" line if you don't want it
+# Returns a string ready to be printed in a form
+
+
+
+sub dropdown {
+  my $inputname = shift;   # Name of the input field, f.ex. 'loc'
+  my $selectedid = shift,  # Id of the initially  selected item, '6'
+  my $selectedname= shift; # Name of the initially selected item, 'Ølbaren'
+  my $options = shift; # List of DIVs to select from
+                #    "<div class='dropdown-item' id='4'>Home</div>\n".
+                #    "<div class='dropdown-item' id='6'>Ølbaren</div>\n" ...
+  my $newfields = shift || ""; # List of input fields for the "new" option
+                # ( "Name", "Address" );  Skip if not wanted
+
+  my $clr = "Onfocus='value=value.trim();select();' autocapitalize='words'";
+
+  my $newdiv = "";
+  if ($newfields) { # We want a way to add new records
+    # Add an option to do so
+    $options = "<div class='dropdown-item' id='new'>(new)</div>\n" . $options;
+    $newdiv = "<div id='newdiv-$inputname' hidden>\n";
+    foreach my $newfield ( @{$newfields} ) {
+      my $pl = $newfield;
+      $pl =~ s/^[a-z]+//; # Skip any prefix line 'new' until uppercase letter
+      $pl =~ s/([A-Z])/ $1/g; # Break words GeoCoord -> Geo Coord
+      $newdiv .= "<input name='$newfield' placeholder='New $pl' $clr /><br/>\n";
+    }
+    $newdiv .= "</div>";
+  }
+
+  my $s = "";
+  $s .= <<JSEND;
+  <style>
+        .dropdown-list {
+            position: absolute;
+            width: 100%;
+            max-height: 200px;
+            overflow-y: auto;
+            border: 1px solid #ccc;
+            z-index: 1000;
+            display: none; /* Hidden by default */
+        }
+        .dropdown-item {
+            cursor: pointer;
+        }
+        .dropdown-item:hover {
+            background-color: #005000;
+        }
+    </style>
+        $newdiv
+        <div id="dropdown-$inputname" style="position:relative;width:100%;max-width:300px;">
+        <input type="text" id="dropdown-input" autocomplete="off"
+          style="width:100%" placeholder='(filter)' value='$selectedname' />
+        <input type="hidden" id='$inputname' name='$inputname' value='$selectedid' >
+        <div id="dropdown-list" class="dropdown-list">
+            $options
+        </div>
+    </div>
+
+    <script>
+        const input = document.getElementById('dropdown-input');
+        const hidinput = document.getElementById('$inputname');
+        const dropdownList = document.getElementById('dropdown-list');
+        const wholedropdown = document.getElementById('dropdown-$inputname');
+        const newdiv = document.getElementById('newdiv-$inputname');
+
+        // Handle selection of a dropdown item
+        dropdownList.addEventListener('click', event => {
+            if (event.target.classList.contains('dropdown-item')) {
+              input.value = event.target.textContent;
+              input.oldvalue = "";
+              hidinput.value = event.target.getAttribute("id");
+              dropdownList.style.display = 'none';
+              if (event.target.getAttribute("id") == "new" ) {
+                console.log("Opening the NEW input");
+                wholedropdown.hidden = true;
+                newdiv.hidden = false;
+              }
+            }
+        });
+
+        // Show/hide dropdown based on input focus
+        input.addEventListener('focus', () => {
+            dropdownList.style.display = 'block';
+            input.oldvalue = input.value;
+            input.value = "";
+        });
+        input.addEventListener('blur', () => {
+            if ( input.oldvalue ) {
+              input.value = input.oldvalue;
+            }
+            // Delay hiding to allow click events on dropdown items
+            setTimeout(() => {
+                dropdownList.style.display = 'none';
+            }, 200);
+        });
+
+        // Filter dropdown items as the user types
+        input.addEventListener('input', () => {
+            const filter = input.value.toLowerCase();
+            Array.from(dropdownList.children).forEach(item => {
+                if (item.textContent.toLowerCase().includes(filter)) {
+                    item.style.display = '';
+                } else {
+                    item.style.display = 'none';
+                }
+            });
+        });
+
+        // Handle Esc to close the dropdown
+        input.addEventListener("keydown", function(event) {
+          if (event.key === "Escape" || event.keyCode === 27) {
+          this.blur();
+          }
+        });
+
+    </script>
+
+JSEND
+  return $s;
+}
+
+
+################################################################################
 # Report module loaded ok
 1;
