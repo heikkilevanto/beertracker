b94bb6815a1fae575bdfbd106fe40bfea9848cc1
Heikki Levanto <heikki@lsd.dk>
2025-04-20 17:48:56 +0200

    Re-add the VERSION.pm
    Signed-off-by: Heikki Levanto <heikki@lsd.dk>

---
+++ VERSION.pm
@@ -0,0 +1,18 @@
+package Version;
+# Auto-generated file. Do not edit !!
+
+use strict;
+use warnings;
+use Exporter 'import';
+
+our %VERSION_INFO = (
+    tag     => 'v2.1',
+    commits => 201,
+    date    => '2025-04-20 15:48:13 UTC',
+    commit  => 'aaa7378',
+    branch  => 'master',
+);
+
+our @EXPORT_OK = qw(%VERSION_INFO);
+
+1;
