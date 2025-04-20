1a4cdb254ed8bfe3c3df90c1c238ab519c6de707
Heikki Levanto <heikki@lsd.dk>
2025-04-20 17:45:08 +0200

    Better version script

--- VERSION.pm
+++ VERSION.pm
@@ -1,3 +1,18 @@
-$version="v2.1"
-$commits="197"
-$date="2025-04-20 16:30:22"
+package Version;
+# Auto-generated file. Do not edit !!
+
+use strict;
+use warnings;
+use Exporter 'import';
+
+our %VERSION_INFO = (
+    tag     => 'v2.1',
+    commits => 199,
+    date    => '2025-04-20 15:44:48 UTC',
+    commit  => 'a9ce37c',
+    branch  => 'master',
+);
+
+our @EXPORT_OK = qw(%VERSION_INFO);
+
+1;
