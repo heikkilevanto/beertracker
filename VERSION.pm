2281946cc07956da21a04167aa39f32b2ebb7080
Heikki Levanto <heikki@lsd.dk>
2025-04-25 15:44:53 +0200

    Remove old record saving routines
    New ones are in the POST section of glasses.pm

--- VERSION.pm
+++ VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v2.9',
-        commits => 8,
-        date    => '2025-04-23 22:15:39 ',
-        commit  => '2ae2595',
+        commits => 9,
+        date    => '2025-04-25 15:44:53 ',
+        commit  => '9507fdf',
         branch  => 'master',
     };
 }
