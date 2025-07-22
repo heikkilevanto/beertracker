ffb629a40de942d272fe147f81069f77d2e2fbd0
Heikki Levanto <heikki@lsd.dk>
2025-07-22 12:27:09 +0200

    Minor

--- VERSION.pm
+++ VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.0',
-        commits => 4,
-        date    => '2025-07-22 12:26:49 ',
-        commit  => '2fd0fde',
+        commits => 5,
+        date    => '2025-07-22 12:27:09 ',
+        commit  => 'dcc9797',
         branch  => 'master',
     };
 }
