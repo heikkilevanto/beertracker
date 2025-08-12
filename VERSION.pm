9ad3384615ea3b928dea63cedaadc9892cc643fd
Heikki Levanto <heikki@lsd.dk>
2025-08-12 11:54:10 +0200

    Add foreign keys to most tables

--- VERSION.pm
+++ VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.0',
-        commits => 58,
-        date    => '2025-08-12 11:53:43 ',
-        commit  => '632dd9a',
+        commits => 59,
+        date    => '2025-08-12 11:54:10 ',
+        commit  => '9d5a470',
         branch  => 'master',
     };
 }
