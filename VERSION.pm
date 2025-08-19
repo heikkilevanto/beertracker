2c17242214c01ba941076dc8763d1e9a07addd3e
Heikki Levanto <heikki@lsd.dk>
2025-08-19 15:26:54 +0200

    Add the new css file

--- VERSION.pm
+++ VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.0',
-        commits => 87,
-        date    => '2025-08-19 14:15:52 ',
-        commit  => 'b9b3bfa',
+        commits => 88,
+        date    => '2025-08-19 15:26:54 ',
+        commit  => '21820bd',
         branch  => 'master',
     };
 }
