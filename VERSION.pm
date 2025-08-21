25c19430a1860147ca57cf057bf30f4bd1d70f47
Heikki Levanto <heikki@lsd.dk>
2025-08-21 13:55:58 +0200

    Fix typo in workflow

--- VERSION.pm
+++ VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.0',
-        commits => 108,
-        date    => '2025-08-21 13:03:56 ',
-        commit  => 'ad9b469',
+        commits => 109,
+        date    => '2025-08-21 13:55:58 ',
+        commit  => '989fbfa',
         branch  => 'master',
     };
 }
