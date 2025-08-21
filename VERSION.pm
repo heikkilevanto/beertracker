989fbfa24a132da677bb2dd1bed46e3f344a6071
Heikki Levanto <heikki@lsd.dk>
2025-08-21 13:03:56 +0200

    No stderr in syntax checks

--- VERSION.pm
+++ VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.0',
-        commits => 107,
-        date    => '2025-08-21 12:57:27 ',
-        commit  => '7ecb0dd',
+        commits => 108,
+        date    => '2025-08-21 13:03:56 ',
+        commit  => 'ad9b469',
         branch  => 'master',
     };
 }
