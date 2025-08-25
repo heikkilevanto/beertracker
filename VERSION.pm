1401e20ad21c45508ab5ed58a0e1784a0c102f58
Heikki Levanto <heikki@lsd.dk>
2025-08-25 12:07:24 +0200

    Fix horizontal scrolling

--- VERSION.pm
+++ VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.0',
-        commits => 123,
-        date    => '2025-08-24 22:25:36 ',
-        commit  => '4711aee',
+        commits => 124,
+        date    => '2025-08-25 12:07:24 ',
+        commit  => '5f2cc83',
         branch  => 'master',
     };
 }
