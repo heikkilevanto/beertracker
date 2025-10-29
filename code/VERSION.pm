2cb1a3bfdd00923686fbf702d5fd1744ea55f41f
Heikki Levanto <heikki@lsd.dk>
2025-10-29 11:59:43 +0100

    MOre of #519: Only list nonzero prices

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.1',
-        commits => 37,
-        date    => '2025-10-29 11:53:59 ',
-        commit  => '990f819',
+        commits => 38,
+        date    => '2025-10-29 11:59:43 ',
+        commit  => 'fa48db9',
         branch  => 'master',
     };
 }
