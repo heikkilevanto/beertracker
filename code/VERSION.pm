7fe8bbb793254029e6f25e2fbc7f4a4a8a8ef897
Heikki Levanto <heikki@lsd.dk>
2025-12-03 16:45:36 +0100

    More #523: Db schema
    FOrgot to commit

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.1',
-        commits => 48,
-        date    => '2025-12-03 16:44:05 ',
-        commit  => '3264b39',
+        commits => 50,
+        date    => '2025-12-03 16:45:36 ',
+        commit  => '53bb1ae',
         branch  => 'master',
     };
 }
