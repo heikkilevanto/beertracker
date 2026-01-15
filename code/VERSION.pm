3b6b8c5688fc11b40b68dbfba65acf4692d3c76a
Heikki Levanto <heikki@lsd.dk>
2026-01-16 00:18:57 +0100

    Add prompt.txt to get the LLM to produce my kind of code

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.1',
-        commits => 64,
-        date    => '2026-01-16 00:10:56 ',
-        commit  => '2b54c0f',
+        commits => 65,
+        date    => '2026-01-16 00:18:57 ',
+        commit  => 'f21ec54',
         branch  => 'master',
     };
 }
