984775b29d47e23a8c45c7f1a3bc3c61f22e7247
Heikki Levanto <heikki@lsd.dk>
2026-01-17 00:24:27 +0100

    rm old file

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.1',
-        commits => 72,
-        date    => '2026-01-17 00:19:11 ',
-        commit  => '5e80f19',
+        commits => 73,
+        date    => '2026-01-17 00:24:27 ',
+        commit  => '611be27',
         branch  => 'master',
     };
 }
