035ef0bf8139d2c33b2e9d7d60881ff437dd091f
Heikki Levanto <heikki@lsd.dk>
2026-01-26 13:21:37 +0100

    V 3.2

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -6,10 +6,10 @@ use warnings;
 
 sub version_info {
     return {
-        tag     => 'v3.1',
-        commits => 138,
-        date    => '2026-01-26 00:23:35 ',
-        commit  => 'b1901b0',
+        tag     => 'v3.2',
+        commits => 0,
+        date    => '2026-01-26 13:21:37 ',
+        commit  => 'af889b6',
         branch  => 'master',
     };
 }
