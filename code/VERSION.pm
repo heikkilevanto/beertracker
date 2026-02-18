a541271d1ae772fe92ac582b5c18289436843b56
Heikki Levanto <heikki@lsd.dk>
2026-02-18 12:30:58 +0100

    ignore the new tmp dir
    where I keep coding plans before pasting them into github issues, and other things I don't want to preserve

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.2',
-        commits => 59,
-        date    => '2026-02-17 22:33:21 ',
-        commit  => '1c0e2bf',
+        commits => 60,
+        date    => '2026-02-18 12:30:58 ',
+        commit  => '9bb7d2e',
         branch  => 'master',
     };
 }
