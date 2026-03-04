88c1adde1351bb6c7659d83818f1d4fc7ffb1562
Heikki Levanto <heikki@lsd.dk>
2026-03-04 19:45:18 +0100

    #566: Log plan

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.2',
-        commits => 115,
-        date    => '2026-03-04 19:33:48 ',
-        commit  => '47baefa',
+        commits => 116,
+        date    => '2026-03-04 19:45:18 ',
+        commit  => '0ac2be3',
         branch  => 'master',
     };
 }
