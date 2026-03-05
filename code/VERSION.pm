6c7c9f40d111fcb2028605cc433b76379a065e20
Heikki Levanto <heikki@lsd.dk>
2026-03-05 15:21:02 +0100

    #565: Switching over to fastcgi
    Should see darker background in dev and prod, once I pull it over

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.2',
-        commits => 124,
-        date    => '2026-03-05 15:10:57 ',
-        commit  => '684a9b5',
+        commits => 125,
+        date    => '2026-03-05 15:21:02 ',
+        commit  => '6adbeeb',
         branch  => 'master',
     };
 }
