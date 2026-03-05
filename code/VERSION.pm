9faee5a2da411bda07d81ef265e6c725230d355c
Heikki Levanto <heikki@lsd.dk>
2026-03-05 19:46:14 +0100

    Increase max size for post data for photos

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.2',
-        commits => 127,
-        date    => '2026-03-05 15:26:20 ',
-        commit  => '66ad9b9',
+        commits => 128,
+        date    => '2026-03-05 19:46:14 ',
+        commit  => 'a937b2b',
         branch  => 'master',
     };
 }
