cc3f1090e342b30f4e4ed08f4f2ae4baaf2d3ceb
Heikki Levanto <heikki@lsd.dk>
2026-03-06 14:27:18 +0100

    Improve dropdowns
    One line per item, scroll sideways with gestures, or shift-scroll on the mouse

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.2',
-        commits => 139,
-        date    => '2026-03-06 14:13:25 ',
-        commit  => '02b06bf',
+        commits => 140,
+        date    => '2026-03-06 14:27:18 ',
+        commit  => 'b50695d',
         branch  => 'master',
     };
 }
