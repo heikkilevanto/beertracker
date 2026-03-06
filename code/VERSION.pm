2af2611ccc15bf6b94c927114955f36b5ea95384
Heikki Levanto <heikki@lsd.dk>
2026-03-06 12:50:51 +0100

    Apache point to code/index

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.2',
-        commits => 133,
-        date    => '2026-03-06 12:48:35 ',
-        commit  => '9f467cc',
+        commits => 134,
+        date    => '2026-03-06 12:50:51 ',
+        commit  => 'b256462',
         branch  => 'master',
     };
 }
