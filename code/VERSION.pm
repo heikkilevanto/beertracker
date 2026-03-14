822d385748f9657031b841a3b072d7f3b68b2123
Heikki Levanto <heikki@lsd.dk>
2026-03-14 23:31:56 +0100

    git ignore the graph

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.2',
-        commits => 198,
-        date    => '2026-03-14 23:29:29 ',
-        commit  => '6b272f6',
+        commits => 199,
+        date    => '2026-03-14 23:31:56 ',
+        commit  => 'a6ec97e',
         branch  => 'master',
     };
 }
