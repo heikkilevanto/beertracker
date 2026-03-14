a6ec97e146fc3ac59745bd627a00cfc03a73660b
Heikki Levanto <heikki@lsd.dk>
2026-03-14 23:29:29 +0100

    Plot estimated hours instead of commits

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.2',
-        commits => 197,
-        date    => '2026-03-14 23:24:53 ',
-        commit  => '86068ab',
+        commits => 198,
+        date    => '2026-03-14 23:29:29 ',
+        commit  => '6b272f6',
         branch  => 'master',
     };
 }
