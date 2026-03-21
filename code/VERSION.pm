3057bd44eb86794af8856ff386c1db2b40f16657
Heikki Levanto <heikki@lsd.dk>
2026-03-21 13:51:37 +0100

    Minor cleaning

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.3',
-        commits => 0,
-        date    => '2026-03-21 12:14:39 ',
-        commit  => 'dfd22d3',
+        commits => 1,
+        date    => '2026-03-21 13:51:37 ',
+        commit  => 'cd609d4',
         branch  => 'master',
     };
 }
