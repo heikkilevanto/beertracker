12b8ca923886a7ae1370d0015adc2aea9e82460a
Heikki Levanto <heikki@lsd.dk>
2026-03-23 18:28:47 +0100

    Clean up the db schema. mov3e tag plan to done

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.3',
-        commits => 8,
-        date    => '2026-03-23 18:18:55 ',
-        commit  => '829aa71',
+        commits => 9,
+        date    => '2026-03-23 18:28:47 ',
+        commit  => 'f9caae3',
         branch  => 'master',
     };
 }
