d3622dd45cd0d777f3d49a25a04c71fd65597f3a
Heikki Levanto <heikki@lsd.dk>
2026-04-11 13:22:24 +0200

    Plan 639 done

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.3',
-        commits => 38,
-        date    => '2026-04-11 13:20:25 ',
-        commit  => '9eeee74',
+        commits => 39,
+        date    => '2026-04-11 13:22:24 ',
+        commit  => '5353c34',
         branch  => 'master',
     };
 }
