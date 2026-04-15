898fccdad35a553e58a01f5d4868d1d2f80d8b62
Heikki Levanto <heikki@lsd.dk>
2026-04-15 15:34:12 +0200

    649 done

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.3',
-        commits => 62,
-        date    => '2026-04-15 15:33:07 ',
-        commit  => '3d39da2',
+        commits => 63,
+        date    => '2026-04-15 15:34:12 ',
+        commit  => '18b74cc',
         branch  => 'master',
     };
 }
