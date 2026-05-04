d00dc171d4a495de3dad9aff888c834aeada6fbd
Heikki Levanto <heikki@lsd.dk>
2026-05-05 00:12:45 +0200

    Move the 665-clean plan files to done

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.3',
-        commits => 122,
-        date    => '2026-05-04 23:06:36 ',
-        commit  => '065d942',
+        commits => 124,
+        date    => '2026-05-05 00:14:07 ',
+        commit  => 'a307d84',
         branch  => 'master',
     };
 }
