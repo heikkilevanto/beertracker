63b593cf4b51d3bb95013265aeafc8437caf7aa0
Heikki Levanto <heikki@lsd.dk>
2026-07-30 16:23:01 +0200

    Fix double scrollbar on pages with listrecords tables
    Prevent viewport scrolling by adding overflow:hidden to html/body,
    and constrain the content-wrapper flex child with min-height:0.
    This ensures only .content-wrapper scrolls, keeping the topline fixed.

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.5',
-        commits => 10,
-        date    => '2026-07-30 14:23:53 ',
-        commit  => '9f7be42',
+        commits => 11,
+        date    => '2026-07-30 16:23:01 ',
+        commit  => 'e45a126',
         branch  => 'master',
     };
 }
