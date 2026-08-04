71b3d0303c6dacfa56a54b6fba9a9e9b1087f379
Heikki Levanto <heikki@lsd.dk>
2026-08-04 11:47:37 +0200

    Restore mobile pull-to-refresh by making body the scroll container
    Revert overflow:hidden on html/body and drop the inner .content-wrapper
    scroll container; pin the topline with position:sticky instead. The
    inner scroller captured the pull gesture, disabling native
    pull-to-refresh on mobile browsers.

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.5',
-        commits => 23,
-        date    => '2026-08-04 11:32:01 ',
-        commit  => '014c8e9',
+        commits => 24,
+        date    => '2026-08-04 11:47:37 ',
+        commit  => 'db0022c',
         branch  => 'master',
     };
 }
