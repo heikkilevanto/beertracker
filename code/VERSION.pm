4483b9dee9468b42989db24b541edb1cdf00db2c
Heikki Levanto <heikki@lsd.dk>
2026-08-11 15:23:38 +0200

    Remove useless plan, already implemented

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.5',
-        commits => 34,
-        date    => '2026-08-11 15:22:26 ',
-        commit  => '451cf37',
+        commits => 35,
+        date    => '2026-08-11 15:23:38 ',
+        commit  => '1f9b802',
         branch  => 'master',
     };
 }
