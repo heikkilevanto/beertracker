78aca96a6f689edf5de7f7269abb966ce9b5f0ed
Heikki Levanto <heikki@lsd.dk>
2026-08-17 13:06:24 +0200

    Remove out-of-date v3.4 release notes

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.5',
-        commits => 55,
-        date    => '2026-08-17 13:05:40 ',
-        commit  => '7e844bc',
+        commits => 56,
+        date    => '2026-08-17 13:06:24 ',
+        commit  => '1aa98ed',
         branch  => 'master',
     };
 }
