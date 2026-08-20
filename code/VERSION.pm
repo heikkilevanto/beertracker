06150665c3b995e4205a84bfdeddfb37ee7bd835
Heikki Levanto <heikki@lsd.dk>
2026-08-20 14:24:21 +0200

    Move 744-barcode plan to done

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.5',
-        commits => 71,
-        date    => '2026-08-20 14:16:33 ',
-        commit  => 'c93286e',
+        commits => 72,
+        date    => '2026-08-20 14:24:21 ',
+        commit  => '5a77332',
         branch  => 'master',
     };
 }
