f34ec9d00f49c3e5c0ccdab0532aaabf5d967b92
Heikki Levanto <heikki@lsd.dk>
2026-08-23 21:07:28 +0200

    Simplify postglass redirect: use util::datestr, include date only for today or earlier

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.5',
-        commits => 84,
-        date    => '2026-08-23 19:57:56 ',
-        commit  => 'bf60576',
+        commits => 85,
+        date    => '2026-08-23 21:07:28 ',
+        commit  => '28588b7',
         branch  => 'master',
     };
 }
--- code/postglass.pm
+++ code/postglass.pm
@@ -178,8 +178,8 @@ sub postglass {
     $glass->{Id}, $c->{username} );
   print { $c->{log} } "Updated glass id '$c->{edit}'\n";
   my ($effdate) = db::queryarray($c, "SELECT strftime('%Y-%m-%d', ?, '-06:00')", $glass->{Timestamp});
-  my ($today) = db::queryarray($c, "SELECT strftime('%Y-%m-%d', 'now', 'localtime', '-06:00')");
-  my $datepart = ($effdate && $effdate ne $today) ? "&date=$effdate" : "";
+  my $today = util::datestr("%F");
+  my $datepart = ($effdate && $effdate ge $today) ? "&date=$effdate" : "";
   $c->{redirect_url} = "$c->{url}?o=$c->{op}$datepart" if $effdate;
 
   } else { # Create a new glass
