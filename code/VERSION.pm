a977d92961975e8b8042bbcd5f12b1a689ee51c6
Heikki Levanto <heikki@lsd.dk>
2026-08-24 11:59:33 +0200

    Move plan 749 to done

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.5',
-        commits => 88,
-        date    => '2026-08-24 11:58:03 ',
-        commit  => '99e7109',
+        commits => 89,
+        date    => '2026-08-24 11:59:33 ',
+        commit  => 'b39f9b6',
         branch  => 'master',
     };
 }
