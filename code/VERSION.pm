31991bad3032d10e65f8cc25ffbf6ecedaaec1c8
Heikki Levanto <heikki@lsd.dk>
2026-08-28 14:07:16 +0200

    Fix color of altbier, tends to be a dark lager

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.5',
-        commits => 108,
-        date    => '2026-08-28 14:04:22 ',
-        commit  => '689ad8d',
+        commits => 110,
+        date    => '2026-08-28 14:15:10 ',
+        commit  => 'ec17bfe',
         branch  => 'master',
     };
 }
