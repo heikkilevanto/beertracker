03fd43711dc6e5d1231a189ce9d969e0cfcedfba
Heikki Levanto <heikki@lsd.dk>
2026-09-07 14:17:30 +0200

    Old plan

--- code/VERSION.pm
+++ code/VERSION.pm
@@ -7,9 +7,9 @@ use warnings;
 sub version_info {
     return {
         tag     => 'v3.5',
-        commits => 133,
-        date    => '2026-09-07 14:13:28 ',
-        commit  => '0bffbe8',
+        commits => 134,
+        date    => '2026-09-07 14:17:30 ',
+        commit  => '5fe77c0',
         branch  => 'master',
     };
 }
